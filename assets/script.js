096242fe49cf7c26f998100084ee9cf1f1cafc96
Christopher Purnell <112015234+Ctpurnell@users.noreply.github.com>
2022-11-30 17:18:24 -0500

    Local Storage

--- assets/script.js
+++ assets/script.js
@@ -5,6 +5,31 @@ var country;
 var lat;
 var lon;
 var queryURL = "https://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + lon + "&appid=" + APIKey;
+var searchBtn = document.querySelector("#searchBtn");
+var formContain = document.querySelector("form-container");
+
+  searchBtn.addEventListener("click", function (event) {
+    var userInput = event.target.previousElementSibling.value;
+    var formContain = event.target.parentElement.getAttribute("id");
+    localStorage.setItem(formContain, userInput);
+
+   });
+
+   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 //fetch(queryURL)//
