e45a6b51e16e9790051c8c80dd3cbc3ac2501038
Christopher Purnell <112015234+Ctpurnell@users.noreply.github.com>
2022-11-30 12:58:40 -0500

    added API key

---
+++ assets/script.js
@@ -0,0 +1,8 @@
+var APIKey = "c32fce2705670ec5d04fdd9e49599a52";
+var city;
+var state;
+var country;
+var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
+
+
+//fetch(queryURL)//
