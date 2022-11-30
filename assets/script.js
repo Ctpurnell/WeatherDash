0dc8dbccf7d1149af164259490bf05897016f8fd
Christopher Purnell <112015234+Ctpurnell@users.noreply.github.com>
2022-11-30 14:30:12 -0500

    changed HTML

--- assets/script.js
+++ assets/script.js
@@ -2,7 +2,9 @@ var APIKey = "c32fce2705670ec5d04fdd9e49599a52";
 var city;
 var state;
 var country;
-var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
+var lat;
+var lon;
+var queryURL = "https://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + lon + "&appid=" + APIKey;
 
 
 //fetch(queryURL)//
